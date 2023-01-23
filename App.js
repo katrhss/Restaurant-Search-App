539a9e872e0db7f25c8b958d0bf37b80302ae38c
Konstantinos Katris <93059645+katrhss@users.noreply.github.com>
2023-01-24 01:42:53 +0200

    Added some stuff

--- App.js
+++ App.js
@@ -2,16 +2,27 @@ import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
 import SearchScreen from "./src/screens/SearchScreen";
 import ResultsShowScreen from "./src/screens/ResultsShowScreen";
+import yelp from "./src/api/yelp";
 
 const navigator = createStackNavigator(
   {
-    Search: SearchScreen,
-    ResultsShow: ResultsShowScreen,
+    Search: {
+      screen: SearchScreen,
+      navigationOptions: {
+        title: "Business Search",
+      },
+    },
+    ResultsShow: {
+      screen: ResultsShowScreen,
+      navigationOptions: {
+        title: "Details",
+      },
+    },
   },
   {
     initialRouteName: "Search",
     defaultNavigationOptions: {
-      title: "Business Search",
+      //title: "Business Search",
     },
   }
 );
--- src/screens/ResultsShowScreen.js
+++ src/screens/ResultsShowScreen.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { View, Text, StyleSheet, FlatList, Image } from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
+
 import yelp from "../api/yelp";
 
 const ResultsShowScreen = ({ navigation }) => {
@@ -18,24 +20,69 @@ const ResultsShowScreen = ({ navigation }) => {
     return null;
   }
 
+  const totalStars = 5;
+  let gainedStars = parseInt(result.rating);
   return (
     <View>
-      <Text>{result.name}</Text>
       <FlatList
+        horizontal
         data={result.photos}
         keyExtractor={(photo) => photo}
         renderItem={({ item }) => {
           return <Image style={styles.image} source={{ uri: item }} />;
         }}
       />
+      <Text style={styles.name}>{result.name}</Text>
+      <Text style={styles.phone}>Phone Number</Text>
+      <Text style={styles.number}>{result.display_phone}</Text>
+      <Text style={styles.rating}>
+        {result.rating} Stars, {result.review_count} Reviews
+      </Text>
+      <Text style={styles.address}>{result.location.address1}</Text>
+      <Text style={styles.address}>
+        {result.location.city}, {result.location.state}{" "}
+        {result.location.zip_code}
+      </Text>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   image: {
-    height: 200,
-    width: 300,
+    height: 260,
+    width: 400,
+    borderRadius: 4,
+    marginHorizontal: 8,
+    marginTop: 10,
+  },
+  name: {
+    fontWeight: "bold",
+    fontSize: 26,
+    marginLeft: 10,
+    marginTop: 10,
+  },
+  rating: {
+    marginLeft: 10,
+    fontSize: 12,
+    marginBottom: 10,
+  },
+  address: {
+    marginLeft: 10,
+    fontSize: 14,
+  },
+  phone: {
+    position: "absolute",
+    top: 340,
+    fontSize: 14,
+    fontWeight: "bold",
+    left: 300,
+  },
+  number: {
+    position: "absolute",
+    top: 355,
+    fontSize: 14,
+    fontStyle: "italic",
+    left: 300,
   },
 });
 
